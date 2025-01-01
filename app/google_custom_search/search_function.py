c3901e2b23bdede5d16ecbed10c8f23dedb192fc
Alex <Sashayerty@ya.ru>
2025-01-02 02:51:07 +0300

    Добавил функцию поиска + поменял структуру логики + добавил reqs

---
+++ app/google_custom_search/search_function.py
@@ -0,0 +1,20 @@
+from googleapiclient.discovery import build
+
+
+def google_search(
+    query: str, api_key: str, cse_id: str, num_results: str = 10
+) -> list:
+    """Функция для поиска ссылок в гугл по query
+
+    Args:
+        query (str): Тема поиска
+        api_key (str): API-ключ
+        cse_id (str): CSE-ключ
+        num_results (str, optional): Количество резов. Defaults to 10.
+
+    Returns:
+        list: Список словарей найденных ссылок. Для извлечения ссылки проходим по списку. Ключи: link, title
+    """
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=query, cx=cse_id, num=num_results).execute()
+    return res["items"]
